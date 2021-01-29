21e1811b055b40f8373df9a55db93210ef8dcdad
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-01-29 16:48:38 -0600

    submit button responding

--- scripts/client.js
+++ scripts/client.js
@@ -1,6 +1,17 @@
 console.log('Hello World');
 $(document).ready(readyOn);
 
+const employeeData = [];
+
+function addEmployeeData(event) {
+  event.preventDefault();
+  console.log('addEmployeeData');
+  // create an object literal to store the data in
+  // push object to the array
+  // clear form
+}
+
 function readyOn() {
   console.log('readyOn');
+  $(document).on('submit', '#submit-employee-info-button', addEmployeeData);
 }
