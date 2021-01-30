dad67ed07f83fda07c2256a181d9de6574e2d69c
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-01-29 18:15:27 -0600

    takes in data, needs to push into array

--- scripts/client.js
+++ scripts/client.js
@@ -6,12 +6,26 @@ const employeeData = [];
 function addEmployeeData(event) {
   event.preventDefault();
   console.log('addEmployeeData');
+  // grab input data from the DOM
+  let firstName = $('#first-name-input').val();
+  let lastName = $('#last-name-input').val();
+  let employeeID = $('#employee-id-input').val();
+  let title = $('#employee-title-input').val();
+  let annualSalary = $('#annual-salary-input').val();
   // create an object literal to store the data in
+  const employee = {
+    firstName: firstName,
+    lastName: lastName,
+    employeeID: employeeID,
+    title: title,
+    annualSalary: Number(annualSalary),
+  };
+  console.log('employee added', employee);
   // push object to the array
   // clear form
 }
 
 function readyOn() {
   console.log('readyOn');
-  $(document).on('submit', '#submit-employee-info-button', addEmployeeData);
+  $(document).on('submit', addEmployeeData);
 }
